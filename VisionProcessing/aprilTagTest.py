452c0c39e8a1c3a1d313bb61fe35f5703276c3d1
MVeszpremi <veszpremimarcell@gmail.com>
2022-11-29 19:01:21 -0800

    New Logic

--- VisionProcessing/aprilTagTest.py
+++ VisionProcessing/aprilTagTest.py
@@ -10,29 +10,76 @@ import serial
 import serial.tools.list_ports
 import time
 
+#global consts
+test_tag_id = 1
+distanceScale = 0.8
+
 cam_id_L = "/dev/video0"
 scale = 1.0
+
+font = cv.FONT_HERSHEY_SIMPLEX
+
+#global vars
+movementDone = True
+hasRotated = False
+
+#functions
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv.resize(frame, dim, interpolation =cv.INTER_AREA)
+
+def send_distance(distance, direction = 'b'):
+    print("[distance]: move - "+ str(distance))
+    distance_send = distance
+    bytes_val = distance_send.to_bytes(2, 'big', signed=True)
+    ser.write(str.encode(direction))
+    ser.write(bytes_val)
+    if(bytes_val == ser.read(2)):
+        print("[distance]: received")
+    else:
+        ser.write(b'\x00') 
+        ser.write(b'\x00') 
+        ser.write(b'\x00') 
+        movementDone = True 
+        time.sleep(0.1)
+
+def send_angle(angle):
+    print("[ angle  ]: move - "+str(angle))
+    bytes_val = angle.to_bytes(2, 'big', signed=True)
+    ser.write(str.encode('d'))
+    ser.write(bytes_val)
+    if(bytes_val == ser.read(2)):
+        print("[ angle  ]: received")
+    else:
+        ser.write(b'\x00') 
+        ser.write(b'\x00') 
+        ser.write(b'\x00') 
+        movementDone = True
+        time.sleep(0.1)
+
+#arduino connect pySerial
 arduino_ports = [
     p.device
     for p in serial.tools.list_ports.comports()
     if ('Arduino' in p.description or 'USB Serial' in p.description)  # may need tweaking to match new arduinos
 ]
 if not arduino_ports:
-    raise IOError("No Arduino found")
+    raise IOError("[arduino ] Not found")
 if len(arduino_ports) > 1:
-    warnings.warn('Multiple Arduinos found - using the first')
-
-
+    warnings.warn('[arduino ] Multiple found - using the first')
 ser = serial.Serial(arduino_ports[0], baudrate = 115200, timeout = 0.5)
 time.sleep(2)
 print(ser.name)         # check which port was really used
 
-
+#video init.
 cap = cv.VideoCapture(cam_id_L)
 if not cap.isOpened():
-    print("Cannot open camera")
+    print("[ camera ] Cannot open")
     exit()
-
+    
+#APTag detector init.
 at_detector = Detector(
    families="tag36h11",
    nthreads=4,
@@ -43,16 +90,6 @@ at_detector = Detector(
    debug=0,
 )
 
-font = cv.FONT_HERSHEY_SIMPLEX
-movementDone = True
-hasRotated = False
-
-
-def rescale_frame(frame, percent=75):
-    width = int(frame.shape[1] * percent/ 100)
-    height = int(frame.shape[0] * percent/ 100)
-    dim = (width, height)
-    return cv.resize(frame, dim, interpolation =cv.INTER_AREA)
 
 start = time.time()
 while True:
@@ -64,70 +101,62 @@ while True:
         gray = rescale_frame(gray, 100/scale)
         result = at_detector.detect(gray, True, (978/scale, 978/scale, 930/scale, 524/scale), 0.175)
         img_size = gray.shape
-        #print("run")
+        
         wid = img_size[1]
         hei = img_size[0]
-        if len(result) > 1:
-            result = result
-        for i in result:
-            cent = i.center
-            Pose_R = i.pose_R
-            Pose_T = i.pose_tspot
-           # print(i)
-            unofficial_tag_position = Pose_T #P @ Pose_R.T @ (-1 * Pose_T)
-            print("relative pos: ")
-            print("x: "+str(unofficial_tag_position[0]) + ", y: "+ str(unofficial_tag_position[1])+ ", z: "+ str(unofficial_tag_position[2]))
-            angle = -1*int(math.atan(unofficial_tag_position[2]/unofficial_tag_position[0])*(180/math.pi))
-            print("angle: "+str(angle))
-            cv.circle(frame,(int(cent[0]), int(cent[1])), 100, (0,0,255), -1)
-            cv.putText(frame, 
-                        str(i.tag_id), 
-                        (int(cent[0]), int(cent[1])), 
-                        font, 3, 
-                        (0, 0, 0), 
-                        10, 
-                        cv.LINE_4)
-            if(movementDone == True and hasRotated == False):
-                movementDone = False
-               # hasRotated = True
-                angle_send = int(90-abs(angle))
+        angleStore = 0
+        distanceStore = 0
+        
+        for i in result: #for each detected aprilTag. 
+            if(i.tag_id == test_tag_id):
+                cent = i.center
+                Pose_R = i.pose_R
+                Pose_T = i.pose_tspot
+                # print(i)
+                unofficial_tag_position = Pose_T #P @ Pose_R.T @ (-1 * Pose_T)
+                # print("relative pos: ")
+                # print("x: "+str(unofficial_tag_position[0]) + ", y: "+ str(unofficial_tag_position[1])+ ", z: "+ str(unofficial_tag_position[2]))
+                angle = -1*int(math.atan(unofficial_tag_position[2]/unofficial_tag_position[0])*(180/math.pi))
+                angle = int(90-abs(angle))
                 if(angle > 0):
-                    angle_send = angle_send * -1
-                print("angleSend: "+str(angle_send))
-                bytes_val = angle_send.to_bytes(2, 'big', signed=True)
-                ser.write(str.encode('d'))
-                ser.write(bytes_val)
-                if(bytes_val == ser.read(2)):
-                    print("received")
-                else:
-                    ser.write(b'\x00') 
-                    ser.write(b'\x00') 
-                    ser.write(b'\x00') 
-                    movementDone = True 
-            if(movementDone == True and hasRotated == True):
-                movementDone = False
-                hasRotated = False
-                distance_send = abs(int(unofficial_tag_position[2]*1000))
-                print("distanceSend: "+str(distance_send))
-                bytes_val = distance_send.to_bytes(2, 'big', signed=True)
-                ser.write(str.encode('b'))
-                ser.write(bytes_val)
-                if(bytes_val == ser.read(2)):
-                    print("received")
-                else:
-                    ser.write(b'\x00')  
-                    movementDone = True 
-                    time.sleep(0.1)
+                    angle = angle * -1
+
+                distance = abs(int(unofficial_tag_position[2]*1000*distanceScale)) 
+
+                angleStore += angle
+                distanceStore += distance
+
+                print("[ angle  ]: "+str(angle))
+                print("[distance]: "+str(distance))
+                #cv.circle(frame,(int(cent[0]), int(cent[1])), 100, (0,0,255), -1)
+                #cv.putText(frame, str(i.tag_id), (int(cent[0]), int(cent[1])), font, 3, (0, 0, 0), 10, cv.LINE_4)
+
+        angleStore = angleStore/len(result)
+        distanceStore = distanceStore/len(result)
+
+        if(movementDone == True and hasRotated == False):
+            movementDone = False
+            # hasRotated = True            
+            send_angle(angleStore)
+        if(movementDone == True and hasRotated == True):
+            movementDone = False
+            hasRotated = False
+            send_distance(distanceStore)
+        
+        angleStore = 0
+        distanceStore = 0
+        
+        #check if movmement is done - arduino sends 0x61 on move done. 
         readS = ser.read(1)
         if(b'\x61' == readS):
-                print("movDone")
+                print("[movement] Done")
+                print()
                 movementDone = True 
         #cv.imshow('frame', frame)
-        if cv.waitKey(1) == ord('q'):
-            break
     except i:
         print(i)
+        break
 # When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+#cv.destroyAllWindows()
 ser.close()             # close port
