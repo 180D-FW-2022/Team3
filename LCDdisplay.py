61c20021376dae3114b69e4aa24cbc494489caef
utkarshkumar7 <utkarshk2002@gmail.com>
2023-03-01 17:07:43 -0800

    Display
    edit display code

--- LCDdisplay.py
+++ LCDdisplay.py
@@ -9,14 +9,8 @@ class LCDdisplay:
         
         #String splitting
         stringLen = len(long_string)
-        
-        if stringLen > 16:
-            top_string = long_string[0:16]
-            bottom_string = long_string[16:]
-            
-        else:
-            top_string = long_string[0:stringLen//2]
-            bottom_string = long_string[stringLen//2:]
+        top_string = long_string[0:stringLen//2]
+        bottom_string = long_string[stringLen//2:]
         
         #Display printing
         mylcd.lcd_display_string(top_string, 1)
